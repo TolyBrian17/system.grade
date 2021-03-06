854ac4121b268785edeccdbe46adf50fdef8b823
Toly Brian <tolybrian17@gmail.com>
2021-03-06 11:54:43 +0300

    update commit

---
+++ ggrading.py
@@ -0,0 +1,30 @@
+for y in range(6):
+    if(y==3 or y==6):
+        continue
+    print(y,end='')
+    print("\n")
+
+
+
+
+grade=int(input("enter score:"))
+if grade >= 70 and grade<=100:
+    print("A")
+if grade >= 60 and grade <= 69:
+            print("B")
+if grade >= 50 and grade <= 59:
+                print("C")
+elif grade >= 40 and grade <= 49:
+                     print("D")
+elif grade >= 0 and grade <= 39:
+                print("E")
+
+
+
+
+
+
+
+
+
+
